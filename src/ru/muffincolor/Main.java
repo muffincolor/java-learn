4dfd6f4e8ba6f1a76e9c6306a2edf9b11182caa1
Дмитрий Кононенко <dkononenko@MacBook-Air-Dmitrij.local>
2021-01-11 20:58:39 +0300

    Реализован весь необходимый функционал

--- src/ru/muffincolor/Main.java
+++ src/ru/muffincolor/Main.java
@@ -3,6 +3,30 @@ package ru.muffincolor;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(calculatePattern(1.0f, 2.3f, 4.3f, 2.3f));
+        System.out.println(checkSum(10, 1));
+        System.out.println(checkNumber(10));
+        sayHello("Дмитрий");
+        System.out.println(checkYear(104));
+    }
 
+    public static float calculatePattern(float a, float b, float c, float d) {
+        return a * (b + (c / d));
+    }
+
+    public static boolean checkSum(int a, int b) {
+        return (a + b >= 10 && a + b <= 20);
+    }
+
+    public static boolean checkNumber(int number) {
+        return number < 0;
+    }
+
+    public static void sayHello(String username) {
+        System.out.println("Привет, " + username + "!");
+    }
+
+    public static boolean checkYear(int year) {
+        return year % 4 == 0 || (year % 100 != 0 && year % 400 == 0);
     }
 }
